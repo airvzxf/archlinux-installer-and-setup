bb01fa301edbc2676da308ff0aa2c27b418d4361
Israel Roldan <israel.alberto.rv@gmail.com>
2017-07-05 20:45:00 -0400

    Upgrade all raw files before I intall my new empty SSD and following the instrucctions.

--- ArchLinuxInstaller/ArchLinuxInstaller.sh
+++ ArchLinuxInstaller/ArchLinuxInstaller.sh
@@ -3,23 +3,13 @@
 # ------------------------------------------------------
 # Arch Linux :: Install
 # ------------------------------------------------------
+# https://github.com/israel-alberto-rv/archLinux-installer-and-setup.git
 
-#tar zxvf install-archLinux.tar.gz ./
-
-
-# FTP Server: ftp.rovisoft.net
-# FTP port: 21
-# User: archLinux@rovisoft.net
-# Pass: ArchLinux830830!
-# ./public_html/project/archLinux
-# /home4/rovisof1/public_html/project/archLinux
-
-# tar -cjvf archLinuxInstaller.tar.bz2 ArchLinuxInstaller
-
-# wget https://project.rovisoft.net/archLinuxInstaller.tar.bz2
-# tar -xjvf archLinuxInstaller.tar.bz2
-
-
+# ------------------------------------------------------
+# NOTE
+# ------------------------------------------------------
+# If the Boot USB with Arch Linux show some error about the sources when you try to instal something, execute this command
+pacman -Syy
 
 
 
@@ -30,29 +20,39 @@
 #Set the keyboard layout
 loadkeys la-latin1
 
+
 # Connect to the Internet
-./wifi-start.sh
+# Select your network and write your password
+wifi-menu
+# Check if your wifi connection works properly
 ping -c3 google.com
 
-# Verify the boot mode
 
+# Verify the boot mode, if it is EFI.
 ls /sys/firmware/efi/efivars
-
 
 # Check if the motherboard works with EFI
 efivar -l
 
+
 # Update the system clock
 timedatectl set-ntp true
 
-# Partition the disks
 
+# Partition the disks
 fdisk -l
 
 # Delete all partitions
+# Warning: Be careful with this command, the meaning of this is if you
+# looking for clean all your disk and install only Arch Linux, otherwise
+# if you have your partition in your disk and you know what are you doing,
+# skipt the gdisk.
 #cfdisk /dev/sda
 
-# Or use gdisk to delete all partitions
+# Use gdisk to delete all partitions
+# Warning: This example delete all partitions and data from the hard disk,
+# then create 3 partitions, first the bios, second the Swap memory
+# (cache of the memory ram), third the partition for all your Arch Linux.
 gdisk
 # -o (delete all)
 # - n
@@ -74,13 +74,16 @@ gdisk
 #   - y
 
 # Format the partitions
+# If you change the order from the partitions in the gdisk, be careful
+# to change the the sda (sda1, sda2, sda3) number in this commands.
 mkfs.fat -F32 /dev/sda1
-mkfs.ext4 /dev/sda3
 mkswap /dev/sda2
 swapon /dev/sda2
+mkfs.ext4 /dev/sda3
 fdisk -l
 
 # Mount the file systems
+# Same careful for this commands.
 mount /dev/sda3 /mnt
 mkdir /mnt/home
 mkdir /mnt/boot
@@ -88,17 +91,21 @@ mkdir /mnt/boot/efi
 mount /dev/sda1 /mnt/boot/efi
 
 
+
 # ------------------------------------------------------
 # Installation
 # ------------------------------------------------------
 
-# Select Mirrors
+# Select mirrors for the database packages
 pacman -S reflector
 cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist-bck
-reflector --country 'United States' --latest 10 --protocol https --sort rate --save /etc/pacman.d/mirrorlist
+# List of countries
+reflector --list-countries
+# Generate a list of mirrors for your contry with 10 servers.
+reflector --country 'US' --latest 10 --protocol https --sort rate --save /etc/pacman.d/mirrorlist
 
 # Download and install the base packages
-pacstrap /mnt /base base-devel
+pacstrap /mnt base base-devel
 
 # Configure the system
 genfstab -U /mnt >> /mnt/etc/fstab
@@ -107,24 +114,26 @@ cat /mnt/etc/fstab
 
 
 # ------------------------------------------------------
-# Arch Root
+# Install Arch Root
 # ------------------------------------------------------
+# This is interesting when you use this command your emulate the login
+# as a root in your new installed partition, every command you run
+# will take effects when you reboot your computer and use your new
+# user and password to login in your account.
 
 # Go to the mnt partition like a Arch Root user.
 arch-chroot /mnt
+
 
 #Set the keyboard layout
 loadkeys la-latin1
 
 # Time zone
-ln -sf /usr/share/zoneinfo/America/New_York/etc/localtime
-
+ln -sf /usr/share/zoneinfo/America/New_York /etc/localtime
 hwclock --systohc
-
 
 # Locale
 # Uncommment en_US.UTF-8 UTF-8
-
 nano /etc/locale.gen
 locale-gen
 echo "LANG=en_US.UTF-8" > /etc/locale.conf
@@ -132,11 +141,11 @@ echo "LANG=en_US.UTF-8" > /etc/locale.conf
 echo "KEYMAP=la-latin1" > /etc/vconsole.conf
 
 # Hostname
-echo "WolfMachine" > /etc/hostname
-echo "127.0.1.1    localhost.localdomain    WolfMachine" >> /etc/hosts
+echo "[your_computer_name]" > /etc/hostname
+echo "127.0.1.1    localhost.localdomain    [your_computer_name]" >> /etc/hosts
 
 # Install Basic Package
-pacman -S wireless_tools wpa_supplicant grub efibootmgr
+pacman -S wifi-menu dialog grub efibootmgr
 
 # Install EFI into Grub
 grub-inatall --tartet=x86_64-efi
@@ -150,16 +159,23 @@ grub-mkconig -o /boot/grub/grub.cfg
 # Root password
 passwd
 
-# Create wol user
-useradd -m -g users -G wheel -s /bin/bash wolf
-passwd wolf
-# Uncomment to allow members of group to execute any command
-EDITOR=nano visudo
-# Copy script for install Xorg and others
-#cp setup-wolf.sh /home/wolf/
-#chown wolf /home/wolf/setup-wolf.sh
+# Create your user
+useradd -m -g users -G wheel -s /bin/bash [your_user_name]
+passwd [your_password_name]
 
-#chgrp users /home/wolf/setup-wolf.sh
+# Add sudo permissions for your user
+# This is the same if you edit the file "nano /etc/sudoers"
+EDITOR=nano visudo
+# And uncomment to allow members of group to execute any command
+# %wheel ALL=(ALL) ALL
+
+
+# Download this project in your computer because when you reboot
+# your computer to the follow steps, you don't have a lot of apps or
+# packages, it's better if you have this information.
+mkdir /home/[your_user_name]/workspace
+cd /home/[your_user_name]/workspace
+git clone https://github.com/israel-alberto-rv/archLinux-installer-and-setup.git
 
 
 # Exit from Arch Root
@@ -168,3 +184,9 @@ exit
 # Reboot
 umount -R /mnt
 reboot
+
+# Check app installer script for examples to how config your Arch Linux,
+# and install Nvidia, Spotify, Steam, Web browser, Utilities, etc.
+
+# The file is in the same project.
+# https://github.com/israel-alberto-rv/archLinux-installer-and-setup.git
--- ArchLinuxInstaller/apps/installer.sh
+++ ArchLinuxInstaller/apps/installer.sh
@@ -10,6 +10,30 @@
 # https://wiki.archlinux.org/index.php/Codecs
 
 
+# Update Repositories
+sudo pacman -Syu
+
+# Uninstall Repositories
+sudo pacman -R #[package_name]
+
+
+# Install yaourt: a pacman frontend which install the AUR packages.
+git clone https://aur.archlinux.org/package-query.git
+cd package-query
+makepkg -si
+cd ..
+git clone https://aur.archlinux.org/yaourt.git
+cd yaourt
+makepkg -si
+cd ..
+
+# Yaourt GUI 
+yaourt -S yaourt-gui
+yaourt -S pamac-aur
+
+
+
+
 # Keyboard configuration in Xorg
 # https://wiki.archlinux.org/index.php/Keyboard_configuration_in_Xorg
 # Dislpay information
@@ -64,6 +88,31 @@ sudo pacman -S --noconfirm reflector
 sudo pacman -S --noconfirm git
 
 
+# Other utilities
+sudo pacman -S xfce4-screenshooter # Screenshots
+
+sudo pacman -S hdparm #List brand and properties for your HHD
+sudo hdparm -I /dev/sda
+
+
+
+
+
+
+# Alsa tutorial
+# https://wiki.gentoo.org/wiki/ALSA
+#-----------------------------------------------------------------------
+sudo pacman -S --noconfirm alsa-utils
+
+# Useful commands
+speaker-test -c2 -twav -l1
+aplay -L
+aplay --list-devices
+amixer controls | grep Master
+cat /sys/class/sound/card*/id
+cat /proc/asound/card0/pcm0p/info
+cat /proc/asound/card0/pcm3p/info
+
 
 
 
@@ -72,6 +121,13 @@ sudo pacman -S --noconfirm epdfview # PDF Viewer
 
 # Development applications
 sudo pacman -S --noconfirm atom
+#useHardwareAcceleration: false
+
+# Bit Torrent
+sudo pacman -S --noconfirm transmission-gtk
+
+
+
 
 # Pale Moon web browser
 cp palemoon/pminstaller.sh ~/
@@ -91,6 +147,8 @@ makepkg -sri
 
 
 
+
+
 # Screen brightness / Backlight
 ls /sys/class/backlight/
 # cat /sys/class/backlight/intel_backlight/max_brightness 
@@ -104,6 +162,25 @@ sudo tee /sys/class/backlight/intel_backlight/brightness <<< 4882
 
 
 
+
+
+# ----------------------------------------------------------------------
+# NVIDIA UPGRADE PACMAN
+# ----------------------------------------------------------------------
+# When upgrade pacman some time we lost the nvidia drivers we need to re-install that.
+sudo pacman -S linux-headers
+
+cd ~/Downloads/
+
+git clone https://aur.archlinux.org/nvidia-utils-beta.git
+cd ~/Downloads/nvidia-utils-beta
+makepkg --check
+makepkg -si
+
+git clone https://aur.archlinux.org/nvidia-beta.git
+cd ~/Downloads/nvidia-beta
+makepkg --check
+makepkg -si
 
 
 
@@ -273,6 +350,50 @@ sudo pacman -S --noconfirm gst-libav
 
 
 
+# Install Boinc
+########################################################################
+sudo pacman -S boinc
+
+# Install libraries
+sudo pacman -S lib32-glibc lib32-glib2 lib32-pango lib32-libxi lib32-mesa lib32-libjpeg6-turbo lib32-libxmu
+
+# Add your user to the boinc group
+sudo usermod -a -G boinc $(whoami)
+# Needs logout and login again
+groups # Checks if you are in the group
+
+# Start the boinc service
+systemctl start boinc.service
+systemctl status boinc.service
+# systemctl restart boinc.service
+# systemctl stop boinc.service
+
+mkdir ~/workspace
+mkdir ~/workspace/boinc
+cd ~/workspace/boinc
+ln -s /var/lib/boinc/gui_rpc_auth.cfg gui_rpc_auth.cfg
+sudo chmod 640 gui_rpc_auth.cfg
+
+boincmgr
+# Add the projects
+https://einsteinathome.org/
+https://boinc.bakerlab.org/rosetta/
+
+
+# Computer 12543238 has been merged successfully into 12467511.
+ 
+
+
+
+boinccmd --lookup_account URL email passwd
+
+# Resources
+# https://boinc.berkeley.edu/wiki/Stop_or_start_BOINC_daemon_after_boot
+# https://www.mankier.com/1/boinccmd
+# https://boinc.berkeley.edu/wiki/Boinccmd_tool
+
+
+
 # Add keyword events
 echo -e '#!/bin/sh\namixer -q sset Master 5%+' | sudo tee /etc/acpi/events/volumeup > /dev/null
 sudo chmod +x /etc/acpi/events/volumeup
@@ -292,9 +413,12 @@ sudo acpid restart
 
 
 
+# List of countries
+reflector --list-countries
 
 # Create Mirror List Server by speed and with the protocol https at least 10 servers
-sudo reflector --country 'United States' --latest 10 --protocol https --sort rate --save /etc/pacman.d/mirrorlist
+sudo reflector --country 'US' --latest 10 --protocol https --sort rate --save /etc/pacman.d/mirrorlist
+
 
 # Force pacman to refresh the package lists
 sudo pacman -Syyu  --noconfirm
@@ -329,3 +453,350 @@ xbindkeys
 # Mute/Unmute volumen
 "amixer -q sset Master toggle"
    XF86AudioMute
+
+
+
+##################################
+# Games
+##################################
+
+# Install a lot of libraries from this AUR package
+yaourt -S wine-gaming-nine
+
+
+# Steam
+# --------------------------------
+# https://wiki.archlinux.org/index.php/Steam
+# https://wiki.archlinux.org/index.php/Steam/Troubleshooting
+# https://wiki.archlinux.org/index.php/Steam/Game-specific_troubleshooting
+# https://wiki.archlinux.org/index.php/Gamepad
+
+# Install Steam
+
+# Easy way
+# --------------------------------
+yaourt -S  steam-native-runtime
+
+# Install OpenSSL 1.0
+yaourt -S lib32-libopenssl-1.0-compat
+# If some game not work, try that first
+# To set custom launch options for a game, right-click on it in your library, select Properties and click on the Set Launch Options button.
+# Add to your launch options:
+# LD_LIBRARY_PATH="/usr/lib:$LD_LIBRARY_PATH"
+# LD_LIBRARY_PATH=/usr/lib/openssl-1.0-compat
+
+# Play with any USB controller on Linux using Steam Big Picture
+# http://steamcommunity.com/app/221410/discussions/0/558748822569010381/
+
+# Play with any USB controller on Linux using xboxdrv to emulate a XBOX controller 
+# https://steamcommunity.com/app/221410/discussions/0/558748653738497361/
+
+
+# Custome way
+
+# https://wiki.archlinux.org/index.php/Steam
+sudo geany /etc/pacman.conf
+# Avoid the hashtags
+[multilib]
+Include = /etc/pacman.d/mirrorlist
+
+sudo pacman -S steam
+
+# Steam/Troubleshooting
+#https://wiki.archlinux.org/index.php/Steam/Troubleshooting#Steam_runtime_issues
+
+# Install steam without UI, then if you run steam-native it show the missing libraries
+steam -textclient
+
+# Run steam, if you got this errors
+# libGL error: No matching fbConfigs or visuals found
+# libGL error: failed to load driver: swrast
+sudo pacman -S lib32-nvidia-utils
+
+
+# Missing libraries
+# ----------------------------------------------------------------------
+cd ~/.local/share/Steam/ubuntu12_32
+file * | grep ELF | cut -d: -f1 | LD_LIBRARY_PATH=. xargs ldd | grep 'not found' | sort | uniq
+
+# All this libraries should be there in MultiLib repositories
+
+# libXtst.so.6 => not found
+sudo pacman -S lib32-libxtst
+
+# libgio-2.0.so.0 => not found
+# libglib-2.0.so.0 => not found
+# libgobject-2.0.so.0 => not found
+sudo pacman -S lib32-libdbusmenu-glib
+
+# libfontconfig.so.1 => not found
+# libfreetype.so.6 => not found
+sudo pacman -S lib32-fontconfig
+
+# libgdk_pixbuf-2.0.so.0 => not found
+sudo pacman -S lib32-gdk-pixbuf2
+
+# libopenal.so.1 => not found
+sudo pacman -S lib32-openal
+
+# libXrandr.so.2 => not found
+sudo pacman -S lib32-libxrandr
+
+# libXinerama.so.1 => not found
+sudo pacman -S lib32-libxinerama
+
+# libusb-1.0.so.0 => not found
+sudo pacman -S lib32-libusb
+
+# libudev.so.0 => not found
+sudo pacman -S lib32-libudev0-shim
+
+# libICE.so.6 => not found
+# libSM.so.6 => not found
+sudo pacman -S lib32-libsm
+
+# libpulse.so.0 => not found
+sudo pacman -S lib32-libpulse
+
+# libdbus-glib-1.so.2 => not found
+# libnm-glib.so.4 => not found
+# libnm-util.so.2 => not found
+sudo pacman -S lib32-libnm-glib
+
+# libgtk-x11-2.0.so.0 => not found
+sudo pacman -S lib32-gtk2
+
+# Others
+sudo pacman -S lib32-libxft
+sudo pacman -S lib32-freetype2
+sudo pacman -S lib32-libpng12 
+
+
+
+
+# Errors and warnings
+# https://wiki.archlinux.org/index.php/Steam/Troubleshooting#Configure_PulseAudio
+# ----------------------------------------------------------------------
+# Solved with the correct configuration in ~/.asoundrc
+
+
+# Other errors
+# ----------------------------------------------------------------------
+# ERROR: ld.so: object '~/.local/share/Steam/ubuntu12_64/gameoverlayrenderer.so' from LD_PRELOAD cannot be preloaded (wrong ELF class: ELFCLASS64): ignored.
+
+# This can be fixed, however, by forcing the game to use a later version of OpenGL than it wants. Right click on the game, select Properties. Then, click "Set Launch Options" in the "General" tab and paste the following: 
+# MESA_GL_VERSION_OVERRIDE=3.1 MESA_GLSL_VERSION_OVERRIDE=140 %command%
+
+# Setup with multiple monitors can cause error which will make game unable to start. If you stuck on this error and have multiple monitors, try to disable all additional displays, and then run a game. You can enable them after the game successfully started. 
+export LD_LIBRARY_PATH=/usr/lib32/nvidia:/usr/lib/nvidia:$LD_LIBRARY_PATH
+
+
+
+# Play with any USB controller on Linux using xboxdrv to emulate a XBOX controller 
+# https://steamcommunity.com/app/221410/discussions/0/558748653738497361/
+# ----------------------------------------------------------------------
+# Install xboxdrv
+# https://aur.archlinux.org/packages/xboxdrv/
+git clone https://aur.archlinux.org/xboxdrv.git
+cd xboxdrv
+sudo pacman -S python2-dbus
+sudo pacman -S scons
+sudo pacman -S boost
+makepkg --check
+makepkg -si
+
+# It should be return nothing
+lsmod | grep xpad
+
+# List all your available input events,
+sudo pacman -S evtest
+ls /dev/input/ | grep event*
+# Try every one from 0 to 20 and press some button in the controller if in the terminal print lines every you press that's the event.
+# PS2 normally is the even 11
+# PS3 normally is the even 13
+sudo evtest /dev/input/event13
+
+# Initializing xboxdrv
+sudo xboxdrv --detach-kernel-driver --mimic-xpad
+xboxdrv --silent --detach-kernel-driver
+
+# Install Joy2Key
+yaourt -S joy2key
+
+
+
+
+# Steam Games
+# ----------------------------------------------------------------------
+
+# Payday 2
+# Game does not start
+# http://steamcommunity.com/app/218620/discussions/22/350544272215417292/
+
+# https://aur.archlinux.org/packages/blt4l-runtime-bin/
+# https://github.com/blt4linux/blt4l
+yaourt -S blt4l-runtime-bin
+
+# No audio
+pacman -Qs pulseaudio
+pacman -Si pulseaudio
+sudo pacman -S pulseaudio
+# And reboot to make effect.
+
+# Wikis
+# http://payday.wikia.com/wiki/Category:PAYDAY_2_heists
+# http://payday.wikia.com/wiki/Weapons_%28Payday_2%29
+# http://payday.wikia.com/wiki/Armors
+# http://payday.wikia.com/wiki/Skills
+# http://payday.wikia.com/wiki/Perk_Decks
+# http://payday.wikia.com/wiki/Category:Special_enemies_%28Payday_2%29
+# http://payday.wikia.com/wiki/Safe_House
+# http://payday.wikia.com/wiki/Gage_Mod_Courier
+
+
+
+# Amnesia: The Dark Descent
+# Tricks and Hacks
+cd ~/.frictionalgames/Amnesia/Main/[userNameInTheGame]
+# Then ls and you see all your save files, open the last one and modify
+# <var type="2" name="mlTinderboxes" val="999" />	Unlimited Tinderboxes
+# <var type="3" name="mfHealth" val="9999.000000" />	Unlimited Health
+# <var type="3" name="mfLampOil" val="9999.000000" />	Unlimited Oil
+# <var type="3" name="mfSanity" val="9999.000000" />	Unlimited Sanity
+
+# Walkthrough
+# https://www.gamefaqs.com/pc/978772-amnesia-the-dark-descent/faqs/60874
+
+
+
+
+# ----------------------------------------------------------------------
+sudo geany /etc/pulse/client.conf
+
+
+
+
+
+# Show the steam installed information
+pacman -Qi steam
+
+# List all installed packages that have the text "nvidia" mentioned somewhere
+pacman -Qs nvidia
+
+sudo pacman -S nvidia
+sudo pacman -S nvidia-libgl
+sudo pacman -S lib32-nvidia-libgl
+
+
+
+# Remove Steam
+sudo pacman -Rs steam
+sudo pacman -Rsc steam
+
+
+# FreedroidRPG — Mature science fiction role playing game set in the future
+sudo pacman -S freedroidrpg
+
+# OpenMW — Attempt to reimplement the popular role-playing game Morrowind. OpenMW aims to be a fully playable, open source implementation of the game's engin
+sudo pacman -S openmw
+
+# Cube 2: Sauerbraten — Improved version of the Cube engine.
+sudo pacman -S sauerbraten
+
+# Urban Terror — Modern multiplayer FPS based on the ioquake3 engine.
+sudo pacman -S urbanterror
+
+# Flight Gear — Open-source, multi-platform flight simulator.
+sudo pacman -S flightgear
+
+# Oolite — 3D space trading and combat simulator in the spirit of Elite.
+sudo pacman -S oolite
+
+# Globulation 2 — Multiplayer RTS with some "economic" elements minimizing the amount of micromanagement.
+sudo pacman -S glob2
+
+# The Battle for Wesnoth — Free, turn-based tactical strategy game with a high fantasy theme, featuring both single-player, and online/hotseat multiplayer combat.
+sudo pacman -S wesnoth
+
+# Simutrans — Another Transport simulation that works on linux with sdl.
+sudo pacman -S simutrans
+
+# OpenTTD — Open source clone of the Microprose game "Transport Tycoon Deluxe", a popular game originally written by Chris Sawyer. It attempts to mimic the original game as closely as possible while extending it with new features.
+sudo pacman -S openttd
+
+# Lincity-ng — City simulation game in which you are required to build and maintain a city. You can win the game either by building a sustainable economy or by evacuating all citizens with spaceships.
+sudo pacman -S lincity-ng
+
+# Dofus — Free, manga inspired, Massively Multiplayer Online Role-playing Game (MMORPG) for Adobe AIR
+sudo pacman -S dofus #AUR
+
+#      Savage 2: A Tortured Soul — Fantasy themed online multiplayer team-based FPS/RTS/RPG hybrid. Free-to-play as of December 2008. Pay for premium accounts providing crucial game elements such as extra inventory slots, access to clans and removal of the Hellbourne unit restrictions (per-account), as well as access to replays and stats.
+sudo pacman -S savage2 #AUR
+
+# Planeshift — Role Playing Game immersed into a 3D virtual fantasy world which is FULLY FREE to play. Fully free means you will have no surprises of premium content which will limit your gameplay or unbalance the game. There are no limitations in skills, ranks, abilities, items you can gain with your free account
+sudo pacman -S planeshift #AUR
+
+# RuneScape — Massive online adventure game by Jagex
+sudo pacman -S runescape-launcher #AUR (new NXT client)
+
+# Ryzom — 3D Fantasy MMORPG
+sudo pacman -S ryzom-client #AUR
+
+
+
+
+
+
+
+
+# Config audio en stereo mode for movies
+# https://bbs.archlinux.org/viewtopic.php?id=167275
+# Replace the file for that
+geany ~/.asoundrc
+
+
+defaults.pcm.rate_converter "samplerate_best"
+
+pcm.snd_card {
+        type hw
+        card 0
+}
+
+pcm.aout {
+        type dmix
+        ipc_key 1
+        ipc_key_add_uid false
+        ipc_perm 0660
+        slave {
+                pcm "snd_card"
+                channels 2
+        }
+}
+
+# Audio in
+pcm.ain {
+        type dsnoop
+        ipc_key 2
+        ipc_key_add_uid false
+        ipc_perm 0660
+        slave {
+                pcm "snd_card"
+                channels 2
+        }
+}
+
+pcm.!surround71 {
+        type vdownmix
+        slave.pcm "aout"
+}
+
+pcm.asymed {
+        type asym
+        playback.pcm "surround71"
+        capture.pcm  "ain"
+}
+
+pcm.!default {
+        type plug
+        slave.pcm "asymed"
+}
