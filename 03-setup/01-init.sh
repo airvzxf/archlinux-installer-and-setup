b838face0df257ecedc9c052a767cf83714a19c4
Israel Roldan <israel.alberto.rv@gmail.com>
2017-07-08 21:04:22 -0400

    Delete 01-intro.sh in directory 03-setup and reorder the files.

--- 03-setup/01-init.sh
+++ 03-setup/01-init.sh
@@ -7,6 +7,27 @@ source 00-config.sh
 # https://github.com/airvzxf/archLinux-installer-and-setup
 
 # ----------------------------------------------------------------------
+# Intro
+# ----------------------------------------------------------------------
+# Looks this documentation for more information about the specific setup
+
+# Installation guide
+# https://wiki.archlinux.org/index.php/Installation_guide
+
+# Laptop main page contains links to article.
+# https://wiki.archlinux.org/index.php/Laptop
+
+# List of applications
+# https://wiki.archlinux.org/index.php/List_of_applications
+
+# Codecs
+# https://wiki.archlinux.org/index.php/Codecs
+
+# Openbox: window manager
+# http://openbox.org/wiki/Help:Getting_started
+
+
+# ----------------------------------------------------------------------
 # Init
 # ----------------------------------------------------------------------
 # This is the basic setup
