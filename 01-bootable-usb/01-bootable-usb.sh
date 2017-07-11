9a55bb91d4f4020f4a0bea1759263a6dbd6af5dd
Israel Roldan <israel.alberto.rv@gmail.com>
2017-07-10 23:08:04 -0400

    Added the first script version to create a bootable USB, I'll test the USB created. Wish me luck!

---
+++ 01-bootable-usb/01-bootable-usb.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+source 00-config.sh
+
+# ----------------------------------------------------------------------
+# Arch Linux :: Bootable USB
+# ----------------------------------------------------------------------
+# https://github.com/airvzxf/archLinux-installer-and-setup
+
+# ----------------------------------------------------------------------
+# Create a bootable USB
+# ----------------------------------------------------------------------
+
+funcContinue() {
+	if ! [[ "$isThisTheUsb" =~ ^([yY][eE][sS]|[yY])+$ ]]; then
+		echo -e "\nThe script has been FINISHED."
+		exit -1
+	fi
+}
+
+
+
+clear
+sudo fdisk -l
+
+echo -e "\n"
+echo -e "Warning: This script delete all partitions and data from the selected device.\n"
+
+read -r -p "Write in lowercase your usb device for example sdb, sdc sdx: " usbDevice
+
+echo -e ""
+read -r -p "Is this (/dev/$usbDevice) the USB device? [y/N]: " isThisTheUsb
+funcContinue $isThisTheUsb
+
+sudo umount -R /dev/$usbDevice &>/dev/null
+
+sudo dd if=/dev/zero of=/dev/$usbDevice bs=512 count=1 conv=notrunc &>/dev/null
+
+(
+	echo o # Create a new empty DOS partition table
+	echo n # Add a new partition
+	echo p # Primary partition
+	echo 1 # Partition number
+	echo   # First sector (Accept default: 1)
+	echo   # Last sector (Accept default: varies)
+	echo a # toggle a bootable flag
+	echo t # change a partition type
+	echo b # b W95 FAT32
+	echo w # Write changes
+) | sudo fdisk /dev/$usbDevice &>/dev/null
+
+usbPartition="$usbDevice"1
+
+echo -e ""
+echo -e "Wait, formatting the USB...\n"
+sudo mkfs.fat -F32 /dev/$usbPartition &>/dev/null
+
+sudo fdisk /dev/$usbDevice -l
+
+mkdir -p ~/workspace
+
+# Needs downlad the last version.
+
+echo -e ""
+echo -e "Wait, loading Arch Linux in the USB...\n"
+sudo dd bs=4M if=~/workspace/archlinux-2017.07.01-x86_64.iso of=/dev/$usbDevice status=progress && sync
