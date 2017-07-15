dd8bbeb4729589dadbba8d57b5108d0db46f2e5b
Israel Roldan <israel.alberto.rv@gmail.com>
2017-07-15 11:43:00 +0000

    Added credentials instructions; Added commands to store your user and password then increment expire time

--- 05-knowledge/99c-git-alias.sh
+++ 05-knowledge/99c-git-alias.sh
@@ -8,6 +8,18 @@
 # ----------------------------------------------------------------------
 # Git
 # ----------------------------------------------------------------------
+# Manually setup these two options
+#git config --global user.email "your_email"
+#git config --global user.name "your_name"
+git config --global -l
+
+# Store your password and specify the time on cache
+git config --global credential.helper cache
+git config --global credential.helper "cache --timeout=3600" # 1 hour
+
+# Store your credentials
+git config credential.helper store
+
 # Create alias to write easy command like git cm thats means git commit -m
 # These configurations will be save in this file ~/.gitconfig
 git config --global --remove-section alias
