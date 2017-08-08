32d5c8e5eec362b0c24731a442aecf5344a9f775
Israel Roldan <wolf@localhost.localdomain>
2017-08-08 19:53:55 -0400

    Improved the pacman instalation, but not solved the problem

--- 04-setup/06-video-games.sh
+++ 04-setup/06-video-games.sh
@@ -14,11 +14,12 @@ echo -e ""
 # Install a lot of libraries for general games from this AUR package
 echo -e "Installing wine gaming libraries"
 echo -e ""
-sudo pacman -S gcc-multilib
-sudo pacman -S gcc-libs-multilib
+sudo pacman -S --needed --noconfirm gcc-multilib
+sudo pacman -S --needed --noconfirm gcc-libs-multilib
 yaourt -S --needed wine-gaming-nine
 echo -e "\n"
 
 
 echo -e "Finished successfully!"
 echo -e ""
+
