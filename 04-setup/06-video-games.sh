808cb840a84a543f34bbf248ad429f65a144de33
Israel Roldan <israel.alberto.rv@gmail.com>
2017-08-08 01:08:49 -0400

    Removed the remove packages comments

--- 04-setup/06-video-games.sh
+++ 04-setup/06-video-games.sh
@@ -14,8 +14,6 @@ echo -e ""
 # Install a lot of libraries for general games from this AUR package
 echo -e "Installing wine gaming libraries"
 echo -e ""
-#sudo pacman -R --noconfirm gcc 2>/dev/null
-#sudo pacman -R --noconfirm gcc-libs
 yaourt -S --needed --depends wine-gaming-nine
 echo -e "\n"
 
