5900540614a33f68e0782496525559b4d422745c
Israel Roldan <wolf@localhost.localdomain>
2017-08-08 20:21:31 -0400

    Improved the style code for install wine gaming replacing the conflicts.

--- 04-setup/06-video-games.sh
+++ 04-setup/06-video-games.sh
@@ -14,7 +14,7 @@ echo -e ""
 # Install a lot of libraries for general games from this AUR package
 echo -e "Installing wine gaming libraries"
 echo -e ""
-sudo bash -c 'yes | pacman -S --needed gcc-multilib'
+yes | sudo pacman -S --needed gcc-multilib
 echo -e "\n"
 yaourt -S --needed --noconfirm wine-gaming-nine
 echo -e "\n"
