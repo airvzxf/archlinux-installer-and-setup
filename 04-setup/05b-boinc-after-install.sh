f2f45a9c78b548c9d2b5461fafcf191309b0f4ea
Israel Roldan <israel.alberto.rv@gmail.com>
2017-08-19 18:43:29 -0400

    Showed boinc groups to validate it is in video group; Improved the setups from boinc data folders; Added in comments 3 new suggested projects.

--- 04-setup/05b-boinc-after-install.sh
+++ 04-setup/05b-boinc-after-install.sh
@@ -16,38 +16,42 @@ echo -e ""
 # After logout and login again
 echo -e "Checks if 'boinc' is in your groups"
 groups
+groups boinc
 echo -e ""
 
 # Boinc work with services to start to use boinc you need to start its
 # services and then open the manager to add, delete project, change
 # configs, etc.
 # Start the boinc service
-echo -e "Start boinc service"
-echo -e ""
-systemctl start boinc.service
-echo -e "\n"
-
-# Other options
-echo -e "Start boinc service"
-echo -e ""
-systemctl status boinc.service
-echo -e "\n"
-#systemctl stop boinc.service
-#systemctl restart boinc.service
+#~ echo -e "Start boinc service"
+#~ echo -e ""
+#~ systemctl start boinc.service
+#~ echo -e "\n"
 
 # If you run the boinc manager you always needs to execute the command
 # in your home directory ~/
-echo -e "Setting up the boinc config to run boinc in the home directory ~/"
-cd ~/
-ln -s /var/lib/boinc/gui_rpc_auth.cfg gui_rpc_auth.cfg
+echo -e "Setting up the boinc config to run boinc in the workspace directory ~/workspace/boinc"
+
+mkdir -p ~/workspace/boinc
+cd ~/workspace/boinc
+
+cp /var/lib/boinc/gui_rpc_auth.cfg gui_rpc_auth.cfg
+sudo chown $(whoami) gui_rpc_auth.cfg
+sudo grp users gui_rpc_auth.cfg
 sudo chmod 640 gui_rpc_auth.cfg
 
-cd ~/ && boincmgr
+/usr/bin/boinc_client &
+boincmgr &
+
+cd ~/
 
 
 # Suggested projects
 # https://einsteinathome.org/
 # https://boinc.bakerlab.org/rosetta/
+# http://www.worldcommunitygrid.org/
+# http://www.gpugrid.net/
+# http://stop.inferia.ru/
 
 # Information
 # https://boinc.berkeley.edu/wiki/Stop_or_start_BOINC_daemon_after_boot
