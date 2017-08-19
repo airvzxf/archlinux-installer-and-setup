3069131c63dd9527078bf21200a8a441b3cb402a
Israel Roldan <israel.alberto.rv@gmail.com>
2017-08-19 18:55:12 -0400

    Added network tools and end message in this script.

--- 04-setup/08-others-suggested.sh
+++ 04-setup/08-others-suggested.sh
@@ -25,3 +25,14 @@ echo -e ""
 sudo pacman -S --needed --noconfirm opencv
 sudo pacman -S --needed --noconfirm opencv-samples
 echo -e "\n"
+
+echo -e ""
+echo -e "Installing Network tools"
+echo -e ""
+sudo pacman -S --needed --noconfirm vnstat #A console-based network traffic monitor
+sudo pacman -S --needed --noconfirm ethtool #Utility to query the network driver and hardware settings
+sudo pacman -S --needed --noconfirm speedtest-cli #Command line interface for testing internet bandwidth using speedtest.net
+echo -e "\n"
+
+echo -e "Finished successfully!"
+echo -e ""
