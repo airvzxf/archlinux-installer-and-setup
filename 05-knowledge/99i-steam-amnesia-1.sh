6ab03c7ed8e1c21c2a3ab2effbf72bd298ffc645
Israel Roldan <israel.alberto.rv@gmail.com>
2017-08-31 22:47:06 -0400

    Added and reorganice the tricks/hacks for Amensia game

--- 05-knowledge/99i-steam-amnesia-1.sh
+++ 05-knowledge/99i-steam-amnesia-1.sh
@@ -12,10 +12,16 @@
 # Tricks and Hacks
 cd ~/.frictionalgames/Amnesia/Main/[userNameInTheGame]
 # Then ls and you see all your save files, open the last one and modify
+
+# Character
+# <class type="cLuxPlayer_SaveData" name="mPlayer">
 # <var type="2" name="mlTinderboxes" val="99" />   Unlimited Tinderboxes
 # <var type="3" name="mfHealth" val="9999.000000" />    Unlimited Health
 # <var type="3" name="mfLampOil" val="9999.000000" />   Unlimited Oil
 # <var type="3" name="mfSanity" val="9999.000000" />    Unlimited Sanity
+
+# Inventory
+# <class type="cLuxInventory_SaveData" name="mInventory">
 
 # Walkthrough
 # https://www.gamefaqs.com/pc/978772-amnesia-the-dark-descent/faqs/60874
