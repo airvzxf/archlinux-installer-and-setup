09bbb311ed188681b979dab77c5d3996d9947198
Israel Roldan <israel.alberto.rv@gmail.com>
2020-11-24 11:36:16 -0600

    Updated the reflector script, now it take the minutes seconds and milliseconds as a number and get the smaller.

--- src/laptop_MSI_GT73EVR_7R_Titan_Pro/04-setup/setup-resources/.reflector_service.sh
+++ src/laptop_MSI_GT73EVR_7R_Titan_Pro/04-setup/setup-resources/.reflector_service.sh
@@ -5,85 +5,258 @@ echo ""
 
 while [ 1 ]
 do
- if ping -c 1 google.com >> /dev/null 2>&1
- then
-  echo -e "~~~~~~~~~~~ RUN THE REFLECTOR PACKAGE ~~~~~~~~~~~"
-  echo -e "*** Yes! The Internet is available. ***"
-  echo -e "*** $(date)  |  $(date -u) ***"
-  echo ""
-
-  FILE="/etc/pacman.d/mirrorlist"
-  FILE_TEMP="/tmp/.mirrorlist-tmp"
-
-  reflector --verbose --threads 8 --country "United States" --protocol https --completion-percent 100 --age 72 --score 8 --sort rate --save "${FILE_TEMP}"
-  echo ""
-  echo ""
-
-  INDEX=0
-  SERVERS=()
-  TIMER=()
-
-  while IFS= read -r LINE
-  do
-    if [[ "${LINE}" == Server* ]]
-    then
-    echo "${LINE}"
-    echo "${LINE}" > "${FILE}"
-    SERVERS[${INDEX}]=$(echo "${LINE}")
-    TIME=$({ time pacman -Syy --noconfirm ; } 2>&1 | grep real | sed "s/real\s*//")
-    TIMER[${INDEX}]=${TIME}
-    echo "INDEX: ${INDEX}"
-    echo "SERVERS[${INDEX}]: ${SERVERS[${INDEX}]}"
-    echo "TIMER[${INDEX}]: ${TIMER[${INDEX}]}"
-    echo ""
-    INDEX=$((INDEX+1))
-    fi
-  done < "${FILE_TEMP}"
-
-  rm -f "${FILE_TEMP}"
-
-  echo ""
-  echo "SERVERS: ${#SERVERS[@]}"
-  echo "TIMER: ${TIMER[@]}"
-
-  if [[ "${INDEX}" -gt 0 ]]
+  if ping -c 1 google.com >> /dev/null 2>&1
   then
+    echo -e "~~~~~~~~~~~ RUN THE REFLECTOR PACKAGE ~~~~~~~~~~~"
+    echo -e "*** Yes! The Internet is available. ***"
+    echo -e "*** $(date)  |  $(date -u) ***"
+    echo ""
+
+    FILE="/etc/pacman.d/mirrorlist"
+    FILE_TEMP="/tmp/.mirrorlist-tmp"
+
+    # --------------------------------------
+    # Reflector: Request the servers and
+    #   stores to the temporal folder.
+    # --------------------------------------
+    reflector --verbose --threads 8 --country "United States" --protocol https --completion-percent 100 --age 72 --score 8 --sort rate --save "${FILE_TEMP}"
     echo ""
     echo ""
-    SMALLEST="${TIMER[0]}"
-    SMALLEST_INDEX=0
-    for (( i=1; i<${INDEX}; i++ ))
+
+    # --------------------------------------
+    # Mirrorlist: Store all the servers and
+    #   download rate into the array.
+    # --------------------------------------
+    INDEX=0
+    SERVERS=()
+    TIMER=()
+
+    while IFS= read -r LINE
     do
-    if [[ "${TIMER[${i}]}" < "${SMALLEST}" ]]
+      if [[ "${LINE}" == Server* ]]
+      then
+        echo "${LINE}"
+        echo "${LINE}" > "${FILE}"
+        SERVERS[${INDEX}]=$(echo "${LINE}")
+        TIME=$({ time pacman -Syy --noconfirm ; } 2>&1 | grep real | sed "s/real\s*//")
+        TIMER[${INDEX}]=${TIME}
+        echo "INDEX: ${INDEX}"
+        echo "SERVERS[${INDEX}]: ${SERVERS[${INDEX}]}"
+        echo "TIMER[${INDEX}]: ${TIMER[${INDEX}]}"
+        echo ""
+        INDEX=$((INDEX+1))
+      fi
+    done < "${FILE_TEMP}"
+
+    rm -f "${FILE_TEMP}"
+
+    echo ""
+    echo "No. of SERVERS: ${#SERVERS[@]}"
+    echo "TIMER:          ${TIMER[@]}"
+    echo ""
+    echo ""
+
+    # --------------------------------------
+    # Array: Get the smallest time.
+    # --------------------------------------
+
+    if [[ "${INDEX}" -gt 0 ]]
     then
-      echo "SMALLEST:"
-      SMALLEST_INDEX=${i}
-      SMALLEST="${TIMER[${i}]}"
+      # --------------------------------------
+      # Real Time: Extract the real time
+      #   separate by minutes.
+      # --------------------------------------
+      INDEX=0
+      MINUTES=()
+
+      echo "# Get minutes:"
+      echo ""
+      for TIME in "${TIMER[@]}"
+      do
+        echo "$TIME"
+        MINUTES[${INDEX}]=$(echo "$TIME" | sed -E "s/([0-9]*)m.*/\1/")
+        INDEX=$(( INDEX + 1 ))
+      done
+      echo ""
+      echo "MINUTES:   ${MINUTES[@]}"
+
+      # --------------------------------------
+      # Real Time: Get the smallest minute and
+      #   create a new array for servers.
+      # --------------------------------------
+      SMALLEST=9999
+
+      for MINUTE in "${MINUTES[@]}"
+      do
+        IS_SMALLER=$(echo "${MINUTE} < ${SMALLEST}" | bc)
+        if [ ${IS_SMALLER} -eq 1 ]
+          then
+            SMALLEST="${MINUTE}"
+          fi
+      done
+      echo "SMALLEST:  ${SMALLEST}"
+
+      INDEX=0
+      NEW_SERVERS=()
+      NEW_TIMER=()
+
+      for MINUTE in "${MINUTES[@]}"
+      do
+        if [[ "${MINUTE}" == "${SMALLEST}" ]]
+          then
+            SMALLEST="${MINUTE}"
+            NEW_SERVERS[${INDEX}]="${SERVERS[${INDEX}]}"
+            NEW_TIMER[${INDEX}]="${TIMER[${INDEX}]}"
+          fi
+          INDEX=$(( INDEX + 1 ))
+      done
+      SERVERS=("${NEW_SERVERS[@]}")
+      TIMER=("${NEW_TIMER[@]}")
+      echo "TIMER:     ${TIMER[@]}"
+      echo""
+      echo""
+
+      # --------------------------------------
+      # Real Time: Extract the real time
+      #   separate by seconds.
+      # --------------------------------------
+      INDEX=0
+      SECONDS=()
+
+      echo "# Get seconds:"
+      echo ""
+      for TIME in "${TIMER[@]}"
+      do
+        echo "$TIME"
+        SECONDS[${INDEX}]=$(echo "$TIME" | sed -E "s/.*m([0-9]*)\..*/\1/")
+        INDEX=$(( INDEX + 1 ))
+      done
+      echo ""
+      echo "SECONDS:   ${SECONDS[@]}"
+
+      # --------------------------------------
+      # Real Time: Get the smallest second and
+      #   create a new array for servers.
+      # --------------------------------------
+      SMALLEST=9999
+
+      for SECOND in "${SECONDS[@]}"
+      do
+        IS_SMALLER=$(echo "${SECOND} < ${SMALLEST}" | bc)
+        if [ ${IS_SMALLER} -eq 1 ]
+          then
+            SMALLEST="${SECOND}"
+          fi
+      done
+      echo "SMALLEST:  ${SMALLEST}"
+
+      INDEX=0
+      NEW_SERVERS=()
+      NEW_TIMER=()
+
+      for SECOND in "${SECONDS[@]}"
+      do
+        if [[ "${SECOND}" == "${SMALLEST}" ]]
+          then
+            SMALLEST="${SECOND}"
+            NEW_SERVERS[${INDEX}]="${SERVERS[${INDEX}]}"
+            NEW_TIMER[${INDEX}]="${TIMER[${INDEX}]}"
+          fi
+          INDEX=$(( INDEX + 1 ))
+      done
+      SERVERS=("${NEW_SERVERS[@]}")
+      TIMER=("${NEW_TIMER[@]}")
+      echo "TIMER:     ${TIMER[@]}"
+      echo""
+      echo""
+
+      # --------------------------------------
+      # Real Time: Extract the real time
+      #   separate by milliseconds.
+      # --------------------------------------
+      INDEX=0
+      MILLISECONDS=()
+
+      echo "# Get milliseconds:"
+      echo ""
+      for TIME in "${TIMER[@]}"
+      do
+        echo "$TIME"
+        MILLISECONDS[${INDEX}]=$(echo "$TIME" | sed -E "s/.*\.([0-9]*)s/\1/")
+        INDEX=$(( INDEX + 1 ))
+      done
+      echo ""
+      echo "MILLISECONDS: ${MILLISECONDS[@]}"
+
+      # --------------------------------------
+      # Real Time: Get the smallest milliseconds
+      #   and create a new array for servers.
+      # --------------------------------------
+      SMALLEST=9999
+
+      for MILLISECOND in "${MILLISECONDS[@]}"
+      do
+        IS_SMALLER=$(echo "${MILLISECOND} < ${SMALLEST}" | bc)
+        if [ ${IS_SMALLER} -eq 1 ]
+          then
+            SMALLEST="${MILLISECOND}"
+          fi
+      done
+      echo "SMALLEST:  ${SMALLEST}"
+
+      INDEX=0
+      NEW_SERVERS=()
+      NEW_TIMER=()
+
+      for MILLISECOND in "${MILLISECONDS[@]}"
+      do
+        if [[ "${MILLISECOND}" == "${SMALLEST}" ]]
+          then
+            SMALLEST="${MILLISECOND}"
+            NEW_SERVERS[${INDEX}]="${SERVERS[${INDEX}]}"
+            NEW_TIMER[${INDEX}]="${TIMER[${INDEX}]}"
+          fi
+          INDEX=$(( INDEX + 1 ))
+      done
+      SERVERS=("${NEW_SERVERS[@]}")
+      TIMER=("${NEW_TIMER[@]}")
+      echo "TIMER:     ${TIMER[@]}"
+      echo""
+      echo""
+
+      # --------------------------------------
+      # Save the best servers in the mirror file.
+      # --------------------------------------
+      echo "" > "${FILE}"
+      for SERVER in "${SERVERS[@]}"
+      do
+        echo "${SERVER}" >> "${FILE}"
+      done
+      echo "SERVERS:   ${SERVERS[@]}"
+      echo""
+
+      # --------------------------------------
+      # Upgrade the packages.
+      # --------------------------------------
+      echo ""
+      time pacman -Syy --noconfirm
+
+      echo ""
+      pacman -Su --noconfirm
+
+      echo ""
+      pacman -Rns $(pacman -Qtdq) --noconfirm
+
+      echo ""
+      sudo -u wolf yay -Sau --noconfirm
     fi
-    echo "${TIMER[${i}]}  ->  ${SERVERS[${i}]}"
-    done
 
-    echo ""
-    echo ""
-    echo "${SERVERS[${SMALLEST_INDEX}]}"
-    echo "${SERVERS[${SMALLEST_INDEX}]}" > "${FILE}"
-
-    echo ""
-    time pacman -Syy --noconfirm
-
-    echo ""
-    pacman -Su --noconfirm
-
-    echo ""
-    pacman -Rns $(pacman -Qtdq) --noconfirm
-
-    echo ""
-    sudo -u wolf yay -Sau --noconfirm
+    break
   fi
 
-  break
- fi
-
- echo "systemctl service, reflector: No internet available."
- sleep 1
+  echo "systemctl service, reflector: No internet available."
+  sleep 1
 done
+
+echo ""
+echo -e "~~~~~~~~~~~ FINISHED THE SERVICE TO RUN REFLECTOR ~~~~~~~~~~~"
