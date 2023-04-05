21f9d10424e53587098f032b39b1b908831bd175
Israel Roldan <israel.alberto.rv@gmail.com>
2023-04-05 08:34:00 -0600

    Removed reload of Zsh with the source command.

--- src/laptop_MSI_GT73EVR_7R_Titan_Pro/04-setup/setup-resources/.p10k.zsh
+++ src/laptop_MSI_GT73EVR_7R_Titan_Pro/04-setup/setup-resources/.p10k.zsh
@@ -24,7 +24,7 @@
 
   # Unset all configuration options. This allows you to apply configuration changes without
   # restarting zsh. Edit ~/.p10k.zsh and type `source ~/.p10k.zsh`.
-  unset -m '(POWERLEVEL9K_*|DEFAULT_USER)~POWERLEVEL9K_GITSTATUS_DIR'
+  #unset -m '(POWERLEVEL9K_*|DEFAULT_USER)~POWERLEVEL9K_GITSTATUS_DIR'
 
   # Zsh >= 5.1 is required.
   autoload -Uz is-at-least && is-at-least 5.1 || return
