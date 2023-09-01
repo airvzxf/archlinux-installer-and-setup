2cfeb8bbabb82ab2a64b6b8f140e4af1a201f3f0
Israel Alberto RV <israel.alberto.rv@gmail.com>
2023-09-01 10:23:29 -0600

    Update 99-user-setup.bash
    Removed the TODO's.

--- src/laptop_MSI_GT73EVR_7R_Titan_Pro/03-installer/99-user-setup.bash
+++ src/laptop_MSI_GT73EVR_7R_Titan_Pro/03-installer/99-user-setup.bash
@@ -19,9 +19,6 @@ mkdir -p ~/.vim
 
 # Move the ArchLinux project into the user folder.
 mkdir --parents ~/workspace/projects
-# TODO: since it is logged with the user, these two lines are not necessary.
-ls -lhaR ~/
-sleep 10
 cd ~/workspace/projects || funcDirectoryNotExist
 
 # ------------------------ #
@@ -37,10 +34,7 @@ git clone https://github.com/airvzxf/archlinux-installer-and-setup.git
 # Set up Git.
 git config --global user.name "${userName}"
 git config --global user.email "${userEmail}"
-# TODO: An error displayed when execute this line. The error is related to root stuff.
 ./archlinux-installer-and-setup/src/laptop_MSI_GT73EVR_7R_Titan_Pro/05-knowledge/99c-git-alias.bash
-# TODO: Check if all is correct.
-sleep 10
 
 # -------- #
 # Finished #
