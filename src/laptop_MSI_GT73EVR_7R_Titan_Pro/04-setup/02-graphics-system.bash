751448cf852466d0035ce0f41c0463c94993f7ca
Israel Roldan <israel.alberto.rv@gmail.com>
2023-12-17 09:10:39 -0600

    Compacted the installation packages.

--- src/laptop_MSI_GT73EVR_7R_Titan_Pro/04-setup/02-graphics-system.bash
+++ src/laptop_MSI_GT73EVR_7R_Titan_Pro/04-setup/02-graphics-system.bash
@@ -18,43 +18,24 @@ funcIsConnectedToInternet
 # Set up the generals #
 # ------------------- #
 
-# Install the X Terminal Emulator.
-optional-packages --install yes xterm
-
 # Install Xorg fonts.
-optional-packages --install yes xorg-fonts-100dpi
-optional-packages --install yes xorg-fonts-75dpi
-optional-packages --install yes xorg-fonts-alias-100dpi
-optional-packages --install yes xorg-fonts-alias-75dpi
-optional-packages --install yes xorg-fonts-alias-cyrillic
-optional-packages --install yes xorg-fonts-alias-misc
-optional-packages --install yes xorg-fonts-cyrillic
-optional-packages --install yes xorg-fonts-encodings
-optional-packages --install yes xorg-fonts-misc
-optional-packages --install yes xorg-fonts-type1
-
-# Install Xorg.
-sudo pacman --sync --needed --noconfirm xorg
-
+# Install the Xorg X server.
 # Install the X.Org initialisation program.
-optional-packages --install yes xorg-xinit
-
 # Install Xorg xrandr.
-optional-packages --install yes xorg-xrandr
-
 # Install the Xorg xdisplay info.
-optional-packages --install yes xorg-xdpyinfo
-
-# Install the Highly configurable and lightweight X11 window manager.
-optional-packages --install yes openbox
-
+# Install the X.Org authorization settings program.
+# Install the lightweight passphrase dialog for SSH.
+# Install the randR-based backlight control application.
+# Install the highly configurable and lightweight X11 window manager.
 # Install the turns on the numlock key in X11.
-optional-packages --install yes numlockx
-
-# Xbindkeys
-# https://wiki.archlinux.org/index.php/Xbindkeys
+# Install the X Terminal Emulator.
 # Install the launch shell commands with your keyboard or your mouse under X.
-optional-packages --install yes xbindkeys
+sudo pacman --sync --needed --noconfirm \
+  xorg-font-util xorg-fonts-alias-100dpi xorg-fonts-100dpi xorg-fonts-alias-75dpi \
+  xorg-fonts-75dpi xorg-fonts-alias-cyrillic xorg-fonts-cyrillic xorg-fonts-alias-misc \
+  xorg-fonts-misc xorg-fonts-encodings xorg-fonts-type1 \
+  xorg-server xorg-server-devel xorg-xinit xorg-xrandr xorg-xdpyinfo xorg-xauth \
+  x11-ssh-askpass xorg-xbacklight openbox numlockx xterm xbindkeys
 
 # Set up the Xbind keys.
 # The command 'xmodmap', shows the keymaps and button mappings.
