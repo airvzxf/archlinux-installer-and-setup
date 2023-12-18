fe36c55ccf5ca27aa6dac0e24c11f17bc231fed0
Israel Roldan <israel.alberto.rv@gmail.com>
2023-12-17 20:40:24 -0600

    Added the bmon package to monitor the usage of the network.

--- src/laptop_MSI_GT73EVR_7R_Titan_Pro/04-setup/01-basic.bash
+++ src/laptop_MSI_GT73EVR_7R_Titan_Pro/04-setup/01-basic.bash
@@ -129,6 +129,7 @@ sed --in-place -- 's/wolf/'"${userId}"'/g' ~/.xinitrc
 # Install a general multilingual speech synthesis system.
 # Install the American male/female and scottish English male speaker.
 # Install the miscellaneous system utilities for Linux.
+# Install the portable bandwidth monitor and rate estimator.
 sudo pacman --sync --needed --noconfirm \
   fontconfig awesome-terminal-fonts gnu-free-fonts noto-fonts noto-fonts-extra noto-fonts-emoji \
   noto-fonts-cjk ttf-bitstream-vera ttf-croscore ttf-dejavu ttf-freefont ttf-droid ttf-ibm-plex \
@@ -139,7 +140,7 @@ sudo pacman --sync --needed --noconfirm \
   hunspell-en_us hunspell-es_mx acpid tmux plocate bc diffutils colordiff hdparm tree strace \
   nmap speedtest-cli iperf bluez bluez-utils zip unzip nvtop android-tools alsa-utils pulseaudio \
   pulseaudio-alsa pulseaudio-bluetooth pulseaudio-equalizer pulseaudio-jack pulsemixer \
-  speech-dispatcher espeak-ng festival festival-us util-linux
+  speech-dispatcher espeak-ng festival festival-us util-linux bmon
 
 # Install the command-line tool that changes the brightness using the xrandr command.
 # Install the tool to generate a sound when the battery is discharging.
