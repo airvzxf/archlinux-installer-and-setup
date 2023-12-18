e226c3def6526c70f23764f923e5ea8242bd4a37
Israel Roldan <israel.alberto.rv@gmail.com>
2023-12-18 10:56:43 -0600

    Added more packages in the suggested installation.

--- src/laptop_MSI_GT73EVR_7R_Titan_Pro/04-setup/06-suggestions.bash
+++ src/laptop_MSI_GT73EVR_7R_Titan_Pro/04-setup/06-suggestions.bash
@@ -19,7 +19,14 @@ funcIsConnectedToInternet
 # -------- #
 
 # Install the pack, ship and run any application as a lightweight container.
-sudo pacman --sync --needed --noconfirm docker
+# Install the Virtualbox host kernel modules for Arch Kernel.
+# Install the official VirtualBox Guest Additions ISO image.
+# Install the VirtualBox Guest userspace utilities.
+# Install the powerful x86 virtualization for enterprise as well as home use.
+sudo pacman --sync --needed --noconfirm \
+  docker virtualbox-host-modules-arch virtualbox-guest-iso virtualbox-guest-utils virtualbox
+
+# Add Docker to the user group.
 sudo usermod -aG docker "${userId}"
 
 # Install a proprietary music streaming service.
